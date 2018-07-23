4e91f9fb5b050d02e51bfd95996126529ab1abc3
Roman Pridybailo <roman.pridybailo@kruschecompany.com>
2018-07-23 18:04:46 +0300

    Added fields

--- report.py
+++ report.py
@@ -13,8 +13,8 @@ file.write("<html>")
 file.write("<body style='background-color:#eeeeee;'>")
 
 file.write('<h1>Vulnerabilities Report</h1>')
-file.write('<table>')
-file.write('<td>Severity</td><td>Vulnerability</td><td>Package</td><td>Curent version</td></td><td>Fixed in version</td>')
+file.write('<table border="1">')
+file.write('<td>Severity</td><td>Vulnerability</td><td>Package</td><td>Curent version</td></td><td>Fixed in version</td><td>OS Version</td')
 for x in data["vulnerabilities"]:
 #  if x['severity'] == "High":
 #  elif x['severity'] == "Critical":
@@ -22,7 +22,7 @@ for x in data["vulnerabilities"]:
 #  elif x['severity'] == "Low":
 #  else:
   file.write('<tr>')
-  file.write("<td>" +  x['severity'] + "</td><td>" + "<a href='" + x['link'] +"'>" +  x['vulnerability']+ "</a></td><td>" + x['featurename'] + "</td><td>" + x['featureversion'] + "</td><td>" + x['fixedby'] + "</td>")
+  file.write("<td>" +  x['severity'] + "</td><td>" + "<a href='" + x['link'] +"'>" +  x['vulnerability']+ "</a></td><td>" + x['featurename'] + "</td><td>" + x['featureversion'] + "</td><td>" + x['fixedby'] + "</td><td>" + x['namespace'] + "</td>")
   file.write('</tr>')
 file.write('</table>')
 file.write("</body>")
