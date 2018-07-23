63dedef70c0d2793626a2c08bf0a834810a9f3ce
Roman Pridybailo <roman.pridybailo@kruschecompany.com>
2018-07-23 17:45:23 +0300

    Remove colors

--- report.py
+++ report.py
@@ -16,7 +16,7 @@ file.write('<h1>Vulnerabilities Report</h1>')
 file.write('<table>')
 file.write('<tr>')
 for x in data["vulnerabilities"]:
-  if x['severity'] == "High":
+#  if x['severity'] == "High":
     file.write("<td>" +  x['severity'] + "</td><td>" + "<a href='" + x['link'] +"'>" +  x['vulnerability']+ "</a></td><td>" + x['namespace'] + "</td><td>" + x['featurename'] + "</td>")
 #  elif x['severity'] == "Critical":
 #    file.write("<p style='color:9F1E03;'>" +  x['severity'] + " " + "<a href='" + x['link'] +"'>" +  x['vulnerability']+ "</a>" + " in " + x['namespace'] + " [" + x['featurename'] + "]" + "</p>")
@@ -24,8 +24,8 @@ for x in data["vulnerabilities"]:
 #    file.write("<p style='color:FFC500;'>" +  x['severity'] + " " + "<a href='" + x['link'] +"'>" +  x['vulnerability']+ "</a>" + " in " + x['namespace'] + " [" + x['featurename'] + "]" + "</p>")
 #  elif x['severity'] == "Low":
 #    file.write("<p style='color:0309DA;'>" +  x['severity'] + " " + "<a href='" + x['link'] +"'>" +  x['vulnerability']+ "</a>" + " in " + x['namespace'] + " [" + x['featurename'] + "]" + "</p>")
-  else:
-    file.write("<p>" +  x['severity'] + " " + "<a href='" + x['link'] +"'>" +  x['vulnerability']+ "</a>" + " in " + x['namespace'] + " [" + x['featurename'] + "]" + "</p>")
+#  else:
+#    file.write("<td>" +  x['severity'] + "</td><td>" + "<a href='" + x['link'] +"'>" +  x['vulnerability']+ "</a></td><td>" + x['namespace'] + "</td><td>" + x['featurename'] + "</td>")
 file.write('</tr>')
 file.write('</table>')
 file.write("</body>")
